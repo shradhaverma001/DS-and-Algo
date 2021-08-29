0235072199a109ec77d4a863d8fbd02c8dfcfcca
shradhaverma001 <shradhaverma001@gmail.com>
2021-08-29 23:26:39 +0530

    delete in LL

---
+++ DS and Algo/Linked_list/deletion.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+};
+
+void InsertBeg(Node** head, int new_data)
+{
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void DeleteNode(Node** head, int key)
+{
+    Node* temp = *head;
+    Node* prev = NULL;
+
+    // if head node has the key(number) to be deleted
+    if (temp != NULL && temp->data == key){
+        *head = temp->next;
+        delete temp;
+        return;
+    }
+    else{
+        while (temp != NULL && temp->data != key)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+        if (temp == NULL)
+            return;
+        prev->next = temp->next;
+        delete temp;
+    }
+
+}
+
+void PrintList(Node* n)
+{
+    while (n!=NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+}
+
+int main()
+{
+    Node* head = NULL;
+    InsertBeg(&head, 7);
+    InsertBeg(&head, 1);
+    InsertBeg(&head, 3);
+    InsertBeg(&head, 2);
+
+    cout << "Created linked list is: " <<endl;
+    PrintList(head);
+    DeleteNode(&head, 1);
+    cout << endl;
+    cout << "Linked List after Deletion of 1: " << endl;
+    PrintList(head);
+    return 0;
+}
