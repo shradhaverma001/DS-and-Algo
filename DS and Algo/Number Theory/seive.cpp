0dbace269e7a6a1fc9d1d1895adad625fa8f9f29
shradhaverma001 <shradhaverma001@gmail.com>
2022-01-21 23:50:17 +0530

    seive of eratosthenes

--- DS and Algo/Number Theory/seive.cpp
+++ DS and Algo/Number Theory/seive.cpp
@@ -3,12 +3,19 @@ using namespace std;
 
 //p[i] = 0 -> prime
 //p[i] = 1 -> non-prime
-int p[10];
-void main()
+int p[12];
+void seive(int n);
+
+int main()
 {
     int n=10;
 
     seive(n);
+
+    for(int i =1; i<=n; i++){
+        cout << i << " " << p[i] << endl;
+    }
+    return 0;
 }
 
 void seive(int n)
@@ -16,9 +23,14 @@ void seive(int n)
     p[0] = 1;
     p[1] = 1;
 
-    for(int i=2; i<n; i++){
+    for(int i=2; i<=n; i++){
         if(p[i] == 0){
+            int j = 2;
 
+            while(i*j <= n){
+                p[i*j] = 1;
+                j++;
+            }
         }
     }
 }
